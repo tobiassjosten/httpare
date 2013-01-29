fe67b75d30f2557373bca7e8eec423ff6030762b
Tobias Sjösten <tobias.sjosten@gmail.com>
2013-01-29 17:19:09 +0100

    Started structuring data types

--- src/Main.hs
+++ src/Main.hs
@@ -2,14 +2,30 @@ module Main where
 
 import Network.HTTP
 
+data Site = Site {
+	domain :: String
+	, protocol :: String
+	, paths :: [String]
+}
+
+data Comparison = Comparison {
+	source :: Site
+	, target :: Site
+	, history :: [String]
+}
+
 main :: IO ()
 main = do
-	response <- Network.HTTP.simpleHTTP (getRequest "http://www.haskell.org/")
+	response <- Network.HTTP.simpleHTTP (getRequest $ url $ source comparison)
 	responseCode <- getResponseCode response
 	putStrLn $ getCodeAsString responseCode
+	where
+		sourceSite = Site "www.haskell.org" "http" ["/"]
+		targetSite = Site "vvv.tobiassjosten.net" "http" []
+		comparison = Comparison sourceSite targetSite []
 
-isSuccess :: ResponseCode -> Bool
-isSuccess (a, _, _) = a == 2
+url :: Site -> String
+url site = protocol site ++ "://" ++ domain site ++ (head $ paths site)
 
 getCodeAsString :: ResponseCode -> String
 getCodeAsString (a, b, c) = show a ++ show b ++ show c
