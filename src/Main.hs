e0b32b3a26a716e9c584ec72a677dbf054e59a8b
Tobias Sjösten <tobias.sjosten@gmail.com>
2013-02-05 21:40:06 +0100

    Simplified in more functional style

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,27 @@
 module Main where
 
-import Httpare.Comparison
 import Network.HTTP
 import System.Environment (getArgs)
 
 main :: IO ()
 main = do
 	args <- getArgs
-	let comparison = makeComparison args
-	response <- Network.HTTP.simpleHTTP (getRequest $ url $ source comparison)
-	responseCode <- getResponseCode response
-	putStrLn $ getCodeAsString responseCode
+	let
+		sourceSite = "http://" ++ (head args)
+		targetSite = "http://" ++ (head $ tail args)
+
+	mapM_ putStrLn $ check targetSite $ check sourceSite ["/"]
+
+check :: String -> [String] -> [String]
+check site [] = []
+check site urls = check' site urls []
+
+check' :: String -> [String] -> [String] -> [String]
+check' site [] newUrls = newUrls
+check' site urls newUrls = newUrls ++ check' site (tail urls) newNewUrls
+	where
+		isSuccessful = True -- Check response for `head urls`
+		newNewUrls = if isSuccessful then newUrls ++ [head urls] else newUrls
 
 getCodeAsString :: ResponseCode -> String
 getCodeAsString (a, b, c) = show a ++ show b ++ show c
